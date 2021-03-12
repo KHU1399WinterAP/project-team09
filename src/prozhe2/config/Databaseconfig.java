33e8bc2171c985accecbc549cb138d76864d0732
unknown <soleimanyali88@gmail.com>
2021-03-12 19:57:12 +0330

    panelo zadim

---
+++ src/prozhe2/config/Databaseconfig.java
@@ -0,0 +1,10 @@
+
+package prozhe2.config;
+
+
+public class Databaseconfig {
+    
+public static final String  connection_url = "jdbc:mysql://localhost:3306/pong";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "jahaniamir.73@gmail.com";
+}
